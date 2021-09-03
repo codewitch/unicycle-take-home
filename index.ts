4daf12e6890de0a2be1e50b7c9bd936deba5936c
Omar White <omawhite@indiana.edu>
2021-09-03 16:57:14 -0500

    trying to figure out rendering block elements

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 // TODO: maybe create types for each type of block elment, since they seem to have different constraints for their children
 interface BlockElement {
   type: "paragraph" | "blockquote" | "list" | "list-item" | "document";
-  children: Array<InlineElement | BlockElement>
+  children: Array<InlineElement> | Array<BlockElement>
 }
 
 interface InlineElement {
@@ -12,16 +12,30 @@ interface InlineElement {
 }
 
 
-export function renderDocument(){
-
+export function renderDocument(document: BlockElement){
+  const {children = []} = document
+  children.forEach((child) => renderBlockElement(child))
 }
 
 
 export function renderBlockElement(element: BlockElement){
-  const {type, children} = element
-
-
-
+  const {type, children = []} = element
+  // if(children.length){
+  //   return children.forEach((child) => renderBlockElement(child))
+  // }
+  // note the way i currently have this is going to cause <p> tags to get rendered twice
+  if(type === 'paragraph'){
+    return`<p>${children.forEach((child) => renderBlockElement(child))}</p>`
+  }
+  //not sure why this for each isn't inferring the type....annoying, TODO
+  // children.forEach((element, _index, _array) => {
+  //   const {type, children} = element
+  //   if(type === 'text' || type === 'link'){
+  //     return renderInlineElement(element)
+  //   }
+  //   // render out other element types
+  //   if(children)
+  // })
 }
 
 
