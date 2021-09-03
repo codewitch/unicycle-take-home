31b21a37b0488456b5e5d14f41cf5a7a8e0ae96f
Omar White <omawhite@indiana.edu>
2021-09-03 16:11:12 -0500

    adds another test case

--- index.test.js
+++ index.test.js
@@ -4,7 +4,11 @@ describe('Tests', () => {
 
 
   describe('renderInlineElement', () => {
-    it('should render links', () => {})
+    it('should render links', () => {
+      const input = { type: "link", content: "link in it", url: "https://example.com" }
+      const result = renderInlineElement(input)
+      expect(result).toEqual(`<a href=${input.url}>${input.content}</a>`)
+    })
     it('should render text', () => {
       const input = { type: "text", content: "This is a paragraph, with a " }
       const result = renderInlineElement(input)
